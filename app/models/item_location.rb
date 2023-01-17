13616f0b05ec0267806235028136d29b99c8eb22
selma <belhadjselma1@gmail.com>
2023-01-17 23:58:58 +0100

    Added the validation to the 3 models

--- app/models/item_location.rb
+++ app/models/item_location.rb
@@ -1,4 +1,6 @@
 class ItemLocation < ApplicationRecord
   belongs_to :item
   belongs_to :location
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true}
+
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,7 @@
 class Location < ApplicationRecord
     has_many :item_locations, dependent: :destroy
     has_many :items, through: :item_locations
+    validates :name, presence: true, length: { minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/,
+        message: "only allows letters" }
+    validates :address, presence: true, length: { minimum: 3 }
 end
