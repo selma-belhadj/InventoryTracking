54938480b210c162fdf786710d17d27e2d734b99
selma <belhadjselma1@gmail.com>
2023-01-17 12:55:06 +0100

    Delete unneeded code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,12 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: %i[ show edit update destroy ]
 
-  # GET /items or /items.json
+  # GET /items 
   def index
     @items = Item.all
   end
 
-  # GET /items/1 or /items/1.json
+  # GET /items/1
   def show
   end
 
@@ -19,41 +19,36 @@ class ItemsController < ApplicationController
   def edit
   end
 
-  # POST /items or /items.json
+  # POST /items 
   def create
     @item = Item.new(item_params)
 
     respond_to do |format|
       if @item.save
         format.html { redirect_to item_url(@item), notice: "Item was successfully created." }
-        format.json { render :show, status: :created, location: @item }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /items/1 or /items/1.json
+  # PATCH/PUT /items/1
   def update
     respond_to do |format|
       if @item.update(item_params)
         format.html { redirect_to item_url(@item), notice: "Item was successfully updated." }
-        format.json { render :show, status: :ok, location: @item }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /items/1 or /items/1.json
+  # DELETE /items/1
   def destroy
     @item.destroy
 
     respond_to do |format|
       format.html { redirect_to items_url, notice: "Item was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,12 +1,12 @@
 class LocationsController < ApplicationController
   before_action :set_location, only: %i[ show edit update destroy ]
 
-  # GET /locations or /locations.json
+  # GET /locations
   def index
     @locations = Location.all
   end
 
-  # GET /locations/1 or /locations/1.json
+  # GET /locations/1
   def show
   end
 
@@ -19,41 +19,36 @@ class LocationsController < ApplicationController
   def edit
   end
 
-  # POST /locations or /locations.json
+  # POST /locations
   def create
     @location = Location.new(location_params)
 
     respond_to do |format|
       if @location.save
         format.html { redirect_to location_url(@location), notice: "Location was successfully created." }
-        format.json { render :show, status: :created, location: @location }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @location.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /locations/1 or /locations/1.json
+  # PATCH/PUT /locations/1
   def update
     respond_to do |format|
       if @location.update(location_params)
         format.html { redirect_to location_url(@location), notice: "Location was successfully updated." }
-        format.json { render :show, status: :ok, location: @location }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @location.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /locations/1 or /locations/1.json
+  # DELETE /locations/1
   def destroy
     @location.destroy
 
     respond_to do |format|
       format.html { redirect_to locations_url, notice: "Location was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
