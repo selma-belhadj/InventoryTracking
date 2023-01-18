4687ef93c6bc943bcda44c2674188c42ef5eefa5
selma <belhadjselma1@gmail.com>
2023-01-18 09:08:36 +0100

    Populate the database with some records

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,6 @@ class Location < ApplicationRecord
     validates :name, presence: true, length: { minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/,
         message: "only allows letters" }
     validates :address, presence: true, length: { minimum: 3 }
+    validates :city, presence: true, length: { minimum: 3 }
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,67 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Location.create([
+    {
+        name: "LocationA",
+        address: "1234 Main St",
+        city: "San Francisco"
+    },
+    {
+        name: "LocationB",
+        address: "1234 Side St",
+        city: "New York"
+    },
+    {
+        name: "LocationC",
+        address: "1234 Back St",
+        city: "Los Angeles"
+    }
+])
+
+items = Item.create([
+    {
+        name: "Phones",
+        description: "Samsung Phones",
+        price: 100
+    },
+    {
+        name: "PCs",
+        description: "Acer Phones",
+        price: 700
+    },
+    {
+        name: "Ipad",
+        description: "Apple Device",
+        price: 500
+    },
+]) 
+
+item_locations = ItemLocation.create([
+    {
+        item_id: 1,
+        location_id: 1,
+        quantity: 10
+    },
+    {
+        item_id: 1,
+        location_id: 2,
+        quantity: 5
+    },
+    {
+        item_id: 2,
+        location_id: 2,
+        quantity: 30
+    },
+    {
+        item_id: 2,
+        location_id: 3,
+        quantity: 20
+    },
+    {
+        item_id: 3,
+        location_id: 1,
+        quantity: 15
+    }
+])
+
